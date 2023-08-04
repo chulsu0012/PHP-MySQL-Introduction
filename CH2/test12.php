b41f3f1866086800a9ec9df7d9f45637989fd415
chulsu0012 <81545875+chulsu0012@users.noreply.github.com>
2023-08-04 18:21:34 +0900

    1주차

---
+++ CH2/test12.php
@@ -0,0 +1,5 @@
+<?php
+    // 리소스
+    $file = fopen("CH2-practice.txt", "r");
+    var_dump($file);
+?>
---
+++ CH2/test8.php
@@ -0,0 +1,8 @@
+<?php      
+    //bool
+    $x = 5 > 3;
+    $y = 5 < 3;
+    var_dump($x);
+    echo "<br>";
+    var_dump($y);
+?>
