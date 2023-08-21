3dd25f66fba685e21e2d8d0e22d6b6e565884448
chulsu0012 <81545875+chulsu0012@users.noreply.github.com>
2023-08-21 20:34:10 +0900

    5주차

---
+++ CH11/member/insert.php
@@ -0,0 +1,21 @@
+<?php
+    $id = $_POST["id"];
+    $pass = $_POST["pass"];
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+
+    $regist_day = date("Y-m-d (H:i)");
+
+    $con = mysqli_connect("localhost", "user", "12345", "sample");
+
+    $sql = "insert into members (id, pass, name, email, regist_day, level, point)";
+    $sql .= "values('$id', '$pass', '$name', '$email', '$regist_day', 9, 0)";
+    
+    mysqli_query($con, $sql);
+    mysqli_close($con);
+
+    // 로그인 폼으로 이동
+    echo "<script>
+        location.href='login_form.php';
+        </script>";
+?>
